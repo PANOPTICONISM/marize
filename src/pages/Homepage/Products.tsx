b871d0a24183f06ba429c52bdde8552e7296804d
krista-t <krista.tomicic@gmail.com>
2021-11-29 12:39:12 +0100

    products fetched

--- src/pages/Homepage/Products.tsx
+++ src/pages/Homepage/Products.tsx
@@ -3,8 +3,15 @@ import { useState, useEffect } from "react";
 import { commerce } from "../../lib/Commerce";
 export default function Products() {
 	const [products, setProducts] = useState([]);
-	//commerce.js method of fetch
-
+	//commerce.product.list() is build in method from commerce
+	const fetchProducts = async () => {
+		const data = await commerce.products.list();
+		setProducts(data);
+	};
+	useEffect(() => {
+		fetchProducts();
+	}, []);
+	console.log(products);
 	return (
 		<div>
 			<h1>Hello World</h1>
