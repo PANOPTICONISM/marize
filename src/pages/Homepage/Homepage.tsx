ab42efab8ea29a72540bb88a889d3b28687b5d49
mariajalmeida <mari9d91@stud.kea.dk>
2021-11-30 21:21:54 +0100

    Assets added and hero responsive.

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Main from "../../containers/Main/Main";
+import Hero from "../../assets/hero-image.png";
+import style from "./homepage.module.css";
 
 export default function Homepage() {
-	return (
-		<>
-			<Main>"This is the homepage"</Main>
-		</>
-	);
+    return (
+        <Main>
+            <header className={style.homepageHero}>
+                <h1 className={style.heading}>
+                    HANDPICKED APPAREL WITH <span>YOU</span> IN MIND
+                </h1>
+                <img src={Hero} alt="hero" />
+            </header>
+        </Main>
+    );
 }
