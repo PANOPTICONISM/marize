b424c699deaad68a0679c7a9fa3dd1b8631d99d5
mariajalmeida <mari9d91@stud.kea.dk>
2021-11-30 17:55:39 +0100

    Navigation added and made responsive.

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import style from "./navigation.module.css";
+import Hamburger from "hamburger-react";
+import SearchBar from "../SearchBar/SearchBar";
+import { AiOutlineUser, AiOutlineHeart } from "react-icons/ai";
+import { BsHandbag } from "react-icons/bs";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
 
-function Navigation() {
-	return <div></div>;
+export default function Navigation() {
+  const [input, setInput] = useState("");
+
+  return (
+    <nav className={style.wrapper}>
+      <div className={style.left_nav}>
+        <Hamburger />
+        <div className={style.search_bar_wrapper}>
+          <SearchBar className={style.search_bar} />
+        </div>
+      </div>
+      <div className={style.logo}>
+        <Logo />
+      </div>
+      <div className={style.right_nav}>
+        <AiOutlineUser />
+        <AiOutlineHeart />
+        <BsHandbag />
+      </div>
+    </nav>
+  );
 }
-
-export default Navigation;
