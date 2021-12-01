e37f209f369ad984ded0e2c613d77512c0a12652
mariajalmeida <mari9d91@stud.kea.dk>
2021-12-01 12:05:40 +0100

    Brand section added.

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import Main from "../../containers/Main/Main";
-import Hero from "../../assets/hero-image.png";
+import hero from "../../assets/hero-image.png";
 import style from "./homepage.module.css";
+import sky from "../../assets/sky-cashmere.svg";
+import caminatta from "../../assets/caminatta.svg";
+import atlanta from "../../assets/atlanta_mocassin.svg";
+import moda from "../../assets/moda_ana.svg";
+import love from "../../assets/love_m.svg";
 
 export default function Homepage() {
     return (
@@ -10,8 +15,15 @@ export default function Homepage() {
                 <h1 className={style.heading}>
                     HANDPICKED APPAREL WITH <span>YOU</span> IN MIND
                 </h1>
-                <img src={Hero} alt="hero" />
+                <img src={hero} alt="hero" />
             </header>
+            <section className={style.brands}>
+                <img src={sky} alt="vera" />
+                <img src={caminatta} alt="caminatta" />
+                <img src={atlanta} alt="atlanta-mocassin" />
+                <img src={moda} alt="moda-ana" />
+                <img src={love} alt="love-m" />
+            </section>
         </Main>
     );
 }
