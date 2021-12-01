150ded01d58409c94c8e6eaf9730aaf13e08a3b5
mariajalmeida <mari9d91@stud.kea.dk>
2021-12-01 18:12:53 +0100

    Button component created.

--- src/components/VisitStore/VisitStore.tsx
+++ src/components/VisitStore/VisitStore.tsx
@@ -1,9 +1,13 @@
+import PrimaryButton from "../Button/Button";
 import style from "./visitstore.module.css";
 
 const VisitStore = () => {
     return (
         <section className={style.visitStore}>
             <h1>Visit us at our store</h1>
+            <PrimaryButton path="./about">
+                LEARN MORE ABOUT MARIZÉ
+            </PrimaryButton>
         </section>
     );
 };
