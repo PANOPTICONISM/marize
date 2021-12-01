a4b7b4f626ba4b55f2483707f0e27b1d54776048
krista-t <krista.tomicic@gmail.com>
2021-12-01 14:06:56 +0100

    sort filter titles added

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,4 +1,8 @@
-import { MdFavoriteBorder } from "react-icons/md";
+import {
+    MdFavoriteBorder,
+    MdKeyboardArrowDown,
+    MdOutlineFilterAlt,
+} from "react-icons/md";
 import style from "./products.module.css";
 import { useCommerceCMS } from "../../contexts/CommerceContext";
 import Main from "../../containers/Main/Main";
@@ -8,27 +12,48 @@ export default function Products() {
 
     return (
         <Main>
-            <h1 className={style.products_title}>clothes for women</h1>
-            <div className={style.products_card}>
-                {products?.map((product) => (
-                    <div className={style.card} key={product["id"]}>
-                        <div className={style.img_container}>
-                            <div className={style.blue_heart}>
-                                <MdFavoriteBorder />
+            <div className="products_container">
+                <header className={style.products_hero}>
+                    <img src="" alt="products_hero" />
+                </header>
+                <h1 className={style.products_title}>clothes for women</h1>
+                <div className={style.sort_filter}>
+                    <p>
+                        women's clothing / brands / flex moda
+                        <p className={style.sort}>
+                            Sort by
+                            <MdKeyboardArrowDown />
+                        </p>
+                        <p className={style.filter}>
+                            <MdOutlineFilterAlt /> All filters
+                        </p>
+                    </p>
+                </div>
+
+                <div className={style.products_card}>
+                    {products?.map((product) => (
+                        <div className={style.card} key={product["id"]}>
+                            <div className={style.img_container}>
+                                <div className={style.blue_heart}>
+                                    <MdFavoriteBorder />
+                                </div>
+                                <img
+                                    src={product["image"]["url"]}
+                                    alt="products"
+                                />
                             </div>
-                            <img src={product["image"]["url"]} alt="products" />
+                            <div className={style.card_txt}>
+                                <p className={style.brand}>
+                                    {product["categories"][1]["name"]}
+                                </p>
+                                <p>{product["name"]}</p>
+                                <p className={style.price}>
+                                    {product["price"]["formatted_with_code"]}
+                                </p>
+                            </div>
                         </div>
-                        <div className={style.card_txt}>
-                            <p className={style.brand}>
-                                {product["categories"][1]["name"]}
-                            </p>
-                            <p>{product["name"]}</p>
-                            <p className={style.price}>
-                                {product["price"]["formatted_with_code"]}
-                            </p>
-                        </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
         </Main>
     );
