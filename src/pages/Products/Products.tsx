8e072bbf62c9bef27e851c9868e0f5f8e89705e8
mariajalmeida <mari9d91@stud.kea.dk>
2021-12-06 21:31:29 +0100

    Lint error fixed.

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -56,6 +56,7 @@ export default function Products() {
             }
         };
         sortArray(sortType);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [sortType]);
 
     const articlesUI = filteredArticles?.map((article: any) => (
