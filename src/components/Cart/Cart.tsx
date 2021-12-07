fc7f2b33bc0cf39d991a1fb473d233a69350fe29
mariajalmeida <mari9d91@stud.kea.dk>
2021-12-07 22:49:52 +0100

    FullCart styled.

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -3,6 +3,8 @@ import { useShoppingBagCMS } from "../../contexts/CartContext";
 import { commerce } from "../../lib/Commerce";
 import style from "./cart.module.css";
 import { BsTrash } from "react-icons/bs";
+import { AiOutlineHeart } from "react-icons/ai";
+import { PrimaryButton } from "../Buttons/Buttons";
 
 export function CartResumeContainer({
     children,
@@ -26,18 +28,34 @@ export function ProductCard({ product }: { product: any }) {
             .then(({ cart }: { cart: any }) => setCart(cart));
     };
 
+    console.log(product);
+
     return (
-        <div>
+        <div className={style.fullCart}>
             <img src={product.image.url} alt={product.name} />
-            <div>
-                <h5>{product.name}</h5>
-                <h6>{product.price.formatted_with_code}</h6>
+            <div className={style.spaceBetween}>
+                <div>
+                    <div className={style.presentation}>
+                        <h4>{product.name}</h4>
+                        <h5>{product.price.formatted_with_code}</h5>
+                    </div>
+                    <p>Colour</p>
+                    <p>Size</p>
+                </div>
+                <div className={style.bottomProduct}>
+                    <div>
+                        <button onClick={removeFromCart}>
+                            <BsTrash />
+                        </button>
+                        <span>||</span>
+                        <button>
+                            <AiOutlineHeart />
+                        </button>
+                    </div>
+                    <div>{product.quantity}</div>
+                </div>
             </div>
-            <p>Colour</p>
-            <p>Size</p>
-            <button onClick={removeFromCart}>
-                <BsTrash />
-            </button>
+            <div></div>
         </div>
     );
 }
@@ -48,15 +66,29 @@ export default function Cart() {
     if (cart && cart.total_unique_items > 0) {
         return (
             <CartResumeContainer>
-                {cart.line_items.map((product: any) => (
-                    <ProductCard product={product} />
+                {cart.line_items?.map((product: any) => (
+                    <>
+                        <ProductCard product={product} />
+                    </>
                 ))}
+                <div className={style.totalCosts}>
+                    <span>Total</span>
+                    <span>{cart.subtotal.formatted_with_code}</span>
+                </div>
+                <PrimaryButton text="Go to your shopping bag" path="/" />
             </CartResumeContainer>
         );
     }
     return (
-        <div className="container cart">
-            <p>Your cart is currently empty.</p>
-        </div>
+        <CartResumeContainer>
+            <div className={style.emptyCart}>
+                <h4>Your shopping bag is currently empty.</h4>
+                <h5>No idea, how to get started?</h5>
+                <PrimaryButton
+                    path="/products"
+                    text="Find out what's new here"
+                />
+            </div>
+        </CartResumeContainer>
     );
 }
