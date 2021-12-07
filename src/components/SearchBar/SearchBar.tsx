faa53502b391e92e7837c19d0f38565344bd3c8a
krista-t <krista.tomicic@gmail.com>
2021-12-07 13:50:32 +0100

    consitionally renders

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -6,19 +6,31 @@ import SearchDropdown from "../SearchDropdown/SearchDropdown";
 const SearchBar = ({ className }: { className?: string }) => {
     const { products } = useCommerceCMS();
     const [search, setSearch] = useState("");
-    const searchArticles = products?.filter((p) => {
-        const searchParam = p.categories.map((c: any) => {
-            return c.name;
+    const [searchedArticles, setSearchedArticles] = useState([]);
+
+    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.target.value);
+        const searchArticles: any = products?.filter((p) => {
+            const searchParam = p.categories.map((c: any) => {
+                return c.name;
+            });
+            return (
+                searchParam
+                    .flat()
+                    .toString()
+                    .toLowerCase()
+                    .indexOf(search.toLowerCase()) > -1
+            );
         });
-        return (
-            searchParam
-                .flat()
-                .toString()
-                .toLowerCase()
-                .indexOf(search.toLowerCase()) > -1
-        );
-    });
-    console.log(searchArticles);
+        console.log(searchArticles, search);
+        if (search.length > 1) {
+            setSearchedArticles(searchArticles);
+        } else {
+            setSearchedArticles([]);
+        }
+        console.log(search.length);
+    };
+
     return (
         <>
             <input
@@ -26,12 +38,10 @@ const SearchBar = ({ className }: { className?: string }) => {
                 type="text"
                 key="1"
                 value={search}
-                placeholder={" search here"}
-                onChange={(e) => setSearch(e.target.value)}
+                placeholder="search here"
+                onChange={(e) => handleSearch(e)}
             />
-            {searchArticles?.length !== 0 && (
-                <SearchDropdown searchArticles={searchArticles} />
-            )}
+            <SearchDropdown searchedArticles={searchedArticles} />
         </>
     );
 };
