19cae3084b21b690eac183aa7ea25270e25b33da
krista-t <krista.tomicic@gmail.com>
2021-12-09 17:31:36 +0100

    categories prepared for menu

--- src/pages/MenuNav/MenuNav.tsx
+++ src/pages/MenuNav/MenuNav.tsx
@@ -1,7 +1,15 @@
 import style from "./menu.module.css";
 import { useState } from "react";
 import { MdHighlightOff } from "react-icons/md";
+import { useCommerceCMS } from "../../contexts/CommerceContext";
 export default function MenuNav() {
+    const {categories } = useCommerceCMS();
+   let navCategories = categories?.filter((cat)=>{
+    return cat.slug !== "accessories"; 
+   }
+    )
+  navCategories = navCategories?.reverse();
+  console.log(navCategories)
     //toggle on menu
     const [isOpen, setIsOpen] = useState(false);
     return (
@@ -10,10 +18,15 @@ export default function MenuNav() {
                 <MdHighlightOff />
             </p>
             <div className={style.nav_wrapper}>
-                <div className={style.nav_title}>
-                    <h5>TITLE</h5>
-                </div>
-                <div className={style.menu_sections}>
+                {navCategories?.map((cats,i)=>
+                  ( <div className={style.nav_title}
+                    key ={i}>
+                        <h5>{cats.name}</h5>
+                    </div>)
+                    
+                )}
+                 </div>
+                {/* <div className={style.menu_sections}>
                     <p>abc</p>
                     <p>abc</p>
                     <p>abc</p>
@@ -21,9 +34,9 @@ export default function MenuNav() {
                     <p>abc</p>
                     <p>abc</p>
                     <p>abc</p>
-                </div>
-            </div>
-            <div className={style.nav_wrapper}>
+                </div> */}
+           
+            {/* <div className={style.nav_wrapper}>
                 <div className={style.nav_title}>
                     <h5>TITLE</h5>
                 </div>
@@ -31,12 +44,18 @@ export default function MenuNav() {
                     <p>abc</p>
                     <p>abc</p>
                 </div>
-            </div>
+            </div> */}
             {/* TODO: COMPONENT IMG HERE */}
             <div className={style.contact_wrapper}>
                 <div className={style.contact_info}>
-                    <div className="facebook">FB</div>
-                    <div className="phone">123</div>
+                    <div className="facebook">
+                        <p>FB</p>
+                    </div>
+                    <div className="phone">
+                        <p>123</p>
+                        <p>123</p>
+                        <p>123</p>
+                    </div>
                     <div className="mail">mail</div>
                     <div className="adress">adress</div>
                     <div className="hours">hours</div>
