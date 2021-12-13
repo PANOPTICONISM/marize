550531a028b837e1df7b071d77d9dcb6a6c0e25c
mariajalmeida <mari9d91@stud.kea.dk>
2021-12-13 21:35:39 +0100

    Content displayed on front-end.

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,5 +1,68 @@
 import React from "react";
+import Main from "../../containers/Main/Main";
+import { useContentfulCMS } from "../../contexts/ContentfulContext";
 
 export default function About() {
-    return <div></div>;
+    const { aboutPage } = useContentfulCMS();
+
+    const {
+        heroImage,
+        title,
+        introduction,
+        secondSectionImage,
+        whyVisitUs,
+        lastSectionTitle,
+        storeImages,
+    } = aboutPage !== undefined && aboutPage[0]?.fields;
+
+    console.log(storeImages);
+    return (
+        <Main>
+            {aboutPage ? (
+                <>
+                    <header>
+                        <img src={heroImage.fields.file.url} alt="" />
+                        <h1>{title}</h1>
+                    </header>
+                    <article>
+                        <section>
+                            <p>{introduction.content[0].content[0].value}</p>
+                        </section>
+                        <section>
+                            <img
+                                src={secondSectionImage.fields.file.url}
+                                alt=""
+                            />
+                            <h2>{whyVisitUs.content[0].content[0].value}</h2>
+                            <ul>
+                                {whyVisitUs.content[1].content.map(
+                                    (content: any) => (
+                                        <li>
+                                            {
+                                                content.content[0].content[0]
+                                                    .value
+                                            }
+                                        </li>
+                                    )
+                                )}
+                            </ul>
+                        </section>
+                        <section>
+                            <h1>{lastSectionTitle}</h1>
+                            <div>
+                                {storeImages.map(
+                                    ({ fields }: { fields?: any }) => (
+                                        <img
+                                            src={fields.file.url}
+                                            alt={fields.title}
+                                        />
+                                    )
+                                )}
+                            </div>
+                        </section>
+                    </article>
+                </>
+            ) : null}
+        </Main>
+    );
 }
