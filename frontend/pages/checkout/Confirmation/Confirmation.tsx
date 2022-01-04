afe46242016f1832ed7b0cd8142985e3dcd94786
panopticonism <panopticonism@gmail.com>
2022-01-04 03:35:17 +0100

    Confirmation page fixed for pre-rendering.

--- frontend/pages/checkout/Confirmation/Confirmation.tsx
+++ frontend/pages/checkout/Confirmation/Confirmation.tsx
@@ -12,90 +12,94 @@ function Confirmation({
 }) {
   const { cart } = useShoppingBagCMS();
   const totalPrice = shippingData?.shippingPrice + cart?.subtotal.raw;
-  return (
-    <div className={style.confirmOrder}>
-      <main>
-        <div className={style.shoppingBag}>
-          <h1>
-            My Shopping Bag
-            <span> ({cart?.total_items} articles)</span>
-          </h1>
-          {cart?.line_items.map((product: any) => (
-            <article key={product.id} className={style.shoppingArticle}>
-              <Image
-                src={product.image.url}
-                width={230}
-                height={300}
-                alt={product.name}
-              />
-              <div className={style.fullSpace}>
-                <div className={style.descDetails}>
-                  <div>
-                    <p>{product.name}</p>
-                    <p>
-                      <span>Size:</span> M
-                    </p>
+  if (shippingData) {
+    return (
+      <div className={style.confirmOrder}>
+        <main>
+          <div className={style.shoppingBag}>
+            <h1>
+              My Shopping Bag
+              <span> ({cart?.total_items} articles)</span>
+            </h1>
+            {cart?.line_items.map((product: any) => (
+              <article key={product.id} className={style.shoppingArticle}>
+                <Image
+                  src={product.image.url}
+                  width={230}
+                  height={300}
+                  alt={product.name}
+                />
+                <div className={style.fullSpace}>
+                  <div className={style.descDetails}>
+                    <div>
+                      <p>{product.name}</p>
+                      <p>
+                        <span>Size:</span> M
+                      </p>
+                    </div>
+                    <p>{product.quantity} pieces</p>
                   </div>
-                  <p>{product.quantity} pieces</p>
+                  <div className={style.flex}>
+                    <span className={style.bagPrice}>20EUR</span>
+                  </div>
                 </div>
-                <div className={style.flex}>
-                  <span className={style.bagPrice}>20EUR</span>
-                </div>
-              </div>
-            </article>
-          ))}
-        </div>
-      </main>
-      <aside>
-        <h2>Order details</h2>
+              </article>
+            ))}
+          </div>
+        </main>
+        <aside>
+          <h2>Order details</h2>
 
-        <h3>Shipping Summary</h3>
+          <h3>Shipping Summary</h3>
 
-        <table>
-          <tbody>
-            <tr>
-              <td>Name</td>
-              <td>{shippingData.firstname + " " + shippingData.lastname}</td>
-            </tr>
-            <tr>
-              <td>Shipping Address</td>
-              <td>{shippingData.address}</td>
-            </tr>
-            <tr>
-              <td>Phone-number</td>
-              <td>{shippingData.phonenumber}</td>
-            </tr>
-            <tr>
-              <td>E-mail</td>
-              <td>{shippingData.email}</td>
-            </tr>
-          </tbody>
-        </table>
-        <h3>Price summary</h3>
-        <table>
-          <tbody>
-            <tr>
-              <td>Subtotal</td>
-              <td>{cart.subtotal.formatted_with_code}</td>
-            </tr>
-            <tr>
-              <td>Shipping</td>
-              <td>{`${shippingData?.shippingPrice} EUR`}</td>
-            </tr>
-          </tbody>
-        </table>
-        <table>
-          <tbody>
-            <tr>
-              <td>Total</td>
-              <td>{`${totalPrice} EUR`}</td>
-            </tr>
-          </tbody>
-        </table>
-        <ContinueButton onClick={next} text="Confirm Order" />
-      </aside>
-    </div>
-  );
+          <table>
+            <tbody>
+              <tr>
+                <td>Name</td>
+                <td>{shippingData.firstname + " " + shippingData.lastname}</td>
+              </tr>
+              <tr>
+                <td>Shipping Address</td>
+                <td>{shippingData.address}</td>
+              </tr>
+              <tr>
+                <td>Phone-number</td>
+                <td>{shippingData.phonenumber}</td>
+              </tr>
+              <tr>
+                <td>E-mail</td>
+                <td>{shippingData.email}</td>
+              </tr>
+            </tbody>
+          </table>
+          <h3>Price summary</h3>
+          <table>
+            <tbody>
+              <tr>
+                <td>Subtotal</td>
+                <td>{cart.subtotal.formatted_with_code}</td>
+              </tr>
+              <tr>
+                <td>Shipping</td>
+                <td>{`${shippingData?.shippingPrice} EUR`}</td>
+              </tr>
+            </tbody>
+          </table>
+          <table>
+            <tbody>
+              <tr>
+                <td>Total</td>
+                <td>{`${totalPrice} EUR`}</td>
+              </tr>
+            </tbody>
+          </table>
+          <ContinueButton onClick={next} text="Confirm Order" />
+        </aside>
+      </div>
+    );
+  }
+
+  return null;
 }
 
 export default Confirmation;
