1dd5f3c4f6aea892381ebb54855c60a7e3198cde
PANOPTICONISM <68908892+PANOPTICONISM@users.noreply.github.com>
2023-10-15 22:19:01 +0200

    HTML semantics.

--- frontend/components/Accordion/Accordion.tsx
+++ frontend/components/Accordion/Accordion.tsx
@@ -3,65 +3,53 @@ import style from "./accordion.module.css";
 import { AiOutlinePlusCircle, AiOutlineMinusCircle } from "react-icons/ai";
 
 export default function Accordion({
-    fields,
+  fields,
 }: {
-    fields?: { question: string; answer: string };
+  fields?: { question: string; answer: string };
 }) {
-    const [isActive, setIsActive] = useState(false);
+  const [isActive, setIsActive] = useState(false);
 
-    return (
-        <div className={style.accordion}>
-            <div className={style.accordionItem}>
-                <div
-                    className={style.accordionTitle}
-                    onClick={() => setIsActive(!isActive)}
-                >
-                    <div>{fields?.question}</div>
-                    <div>
-                        {isActive ? (
-                            <AiOutlineMinusCircle />
-                        ) : (
-                            <AiOutlinePlusCircle />
-                        )}
-                    </div>
-                </div>
-                {isActive && (
-                    <div className={style.accordionContent}>
-                        {fields?.answer}
-                    </div>
-                )}
-            </div>
-        </div>
-    );
+  return (
+    <div className={style.accordion}>
+      <div className={style.accordionItem}>
+        <button
+          className={style.accordionTitle}
+          onClick={() => setIsActive(!isActive)}
+        >
+          <div>{fields?.question}</div>
+          <div>
+            {isActive ? <AiOutlineMinusCircle /> : <AiOutlinePlusCircle />}
+          </div>
+        </button>
+        {isActive && (
+          <div className={style.accordionContent}>{fields?.answer}</div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export function AccordionDetails({ fields }: { fields?: any }) {
-    const [isActive, setIsActive] = useState(false);
+  const [isActive, setIsActive] = useState(false);
 
-    const removeHTMLContent = fields?.description.replace(/(<([^>]+)>)/gi, "");
+  const removeHTMLContent = fields?.description.replace(/(<([^>]+)>)/gi, "");
 
-    return (
-        <div className={`${style.accordion} ${style.accordionDetails}`}>
-            <div className={style.accordionItem}>
-                <div
-                    className={style.accordionTitle}
-                    onClick={() => setIsActive(!isActive)}
-                >
-                    <div>DESCRIPTION</div>
-                    <div>
-                        {isActive ? (
-                            <AiOutlineMinusCircle />
-                        ) : (
-                            <AiOutlinePlusCircle />
-                        )}
-                    </div>
-                </div>
-                {isActive && (
-                    <div className={style.accordionContent}>
-                        {removeHTMLContent}
-                    </div>
-                )}
-            </div>
-        </div>
-    );
+  return (
+    <div className={`${style.accordion} ${style.accordionDetails}`}>
+      <div className={style.accordionItem}>
+        <button
+          className={style.accordionTitle}
+          onClick={() => setIsActive(!isActive)}
+        >
+          <div>DESCRIPTION</div>
+          <div>
+            {isActive ? <AiOutlineMinusCircle /> : <AiOutlinePlusCircle />}
+          </div>
+        </button>
+        {isActive && (
+          <div className={style.accordionContent}>{removeHTMLContent}</div>
+        )}
+      </div>
+    </div>
+  );
 }
